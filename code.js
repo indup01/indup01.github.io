a45b65dad22297001487def28c72692fc294f73a
indup01 <115511152+indup01@users.noreply.github.com>
2023-05-25 06:06:53 +0100

    Add files via upload

---
+++ code.js
@@ -0,0 +1,20 @@
+function fun() {
+    alert("Syncing Now")
+
+}
+//     $connection = mysql_connect('localhost', 'root', '12345'); //The Blank string is the password
+//     mysql_select_db('pilldata');
+//
+//     $query = "SELECT * FROM employee"; //You don't need a ; like you do in SQL
+//     $result = mysql_query($query);
+//
+//     echo "<table>"; // start a table tag in the HTML
+//
+//     while($row = mysql_fetch_array($result)){   //Creates a loop to loop through results
+//         echo "<tr><td>" . htmlspecialchars($row['name']) . "</td><td>" . htmlspecialchars($row['age']) . "</td></tr>";  //$row['index'] the index here is a field name
+//     }
+//
+//     echo "</table>"; //Close the table in HTML
+//
+//     mysql_close(); //Make sure to close out the database connection
+// }
